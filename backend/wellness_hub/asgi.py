117eaa186ac21f3880dc336f537ba41e8c1c01ee
Willow Hart <203877208@qq.com>
2025-11-11 19:28:30 +0800

    update

--- backend/wellness_hub/asgi.py
+++ backend/wellness_hub/asgi.py
@@ -3,13 +3,16 @@ ASGI config for wellness_hub project.
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wellness_hub.settings')
+
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 import apps.chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wellness_hub.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
